a148921effda53228d6582b87ad41af324a14b7f
alihamzarao6 <alihamzarao6@gmail.com>
2025-05-27 14:58:59 +0500

    refactor: Clean up app.js and server.js; streamline middleware usage and error handling

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
 const mongoose = require("mongoose");
-const logger = require("./utils/logger");
 const config = require("./config/config");
 
 const PORT = process.env.PORT || 5000;
@@ -38,12 +37,12 @@ mongoose
 
 // Handle uncaught exceptions
 process.on("uncaughtException", (err) => {
-  logger.error("Uncaught Exception:", err);
+  console.error("Uncaught Exception:", err);
   process.exit(1);
 });
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err) => {
-  logger.error("Unhandled Rejection:", err);
+  console.error("Unhandled Rejection:", err);
   process.exit(1);
 });
